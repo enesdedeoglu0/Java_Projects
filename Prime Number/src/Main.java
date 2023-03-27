39fc1c6da0dd44fc45ce54c14180e2aec8852066
ENES DEDEOĞLU <101185956+enesdedeoglu0@users.noreply.github.com>
2023-03-27 15:07:37 +0300

    Add files via upload

---
+++ Prime Number/src/Main.java
@@ -0,0 +1,19 @@
+//ENES DEDEOĞLU 27.03.2023
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            int n = 0;
+            for (int k = 1; k <= i; k++) {
+                if (i % k == 0) {
+
+                    n++;
+                }
+            }
+            if (n<=2){
+                System.out.println(i);
+            }
+        }
+
+
+    }
+}
