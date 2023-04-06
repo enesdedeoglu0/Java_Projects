d768d3444c46485a6d29e7998ba4bdd1b9d85bea
ENES DEDEOĞLU <101185956+enesdedeoglu0@users.noreply.github.com>
2023-04-06 15:54:59 +0300

    Add files via upload

---
+++ Employee/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee person1= new Employee("ENES ", 2000, 45,1985);
+
+        System.out.println(person1.toString());
+
+    }
+}
